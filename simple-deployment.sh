ef14025e9bfc716ba09004cbc93d5316e75100b0
QuantumTacoNinja <worapat1h@gmail.com>
2025-12-19 11:26:44 +0700

    Update simple-deployment.sh

--- simple-deployment.sh
+++ simple-deployment.sh
@@ -7,10 +7,10 @@ NODE_VERSION="24.x"
 ssh -o StrictHostKeyChecking=no -i ${FILENAME} ${USERNAME}@${TARGET_IP} "echo hello"
 
 # Sync node packages.
-scp -o StrictHostKeyChecking=no -i $FILENAME index.js '$USERNAME@$TARGET_HOST:~/index.js'
-scp -o StrictHostKeyChecking=no -i $FILENAME package.json '$USERNAME@$TARGET_HOST:~/package.json'
-scp -o StrictHostKeyChecking=no -i $FILENAME package-lock.json '$USERNAME@$TARGET_HOST:~/package-lock.json'
-scp -o StrictHostKeyChecking=no -i "$FILENAME" -r node_modules '$USERNAME@$TARGET_HOST:~/node_modules'
+scp -o StrictHostKeyChecking=no -i $FILENAME index.js ${USERNAME}@${TARGET_HOST}:~/index.js
+scp -o StrictHostKeyChecking=no -i $FILENAME package.json ${USERNAME}@${TARGET_HOST}:~/package.json
+scp -o StrictHostKeyChecking=no -i $FILENAME package-lock.json ${USERNAME}@${TARGET_HOST}:~/package-lock.json
+scp -o StrictHostKeyChecking=no -i $FILENAME -r node_modules ${USERNAME}@${TARGET_HOST}:~/node_modules
 
 # Install nodejs and dependencies.
 ssh -o StrictHostKeyChecking=no -i $FILENAME '$USERNAME@$TARGET_HOST' "
