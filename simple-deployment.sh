388ecf69b9cb7cbcace63af2dfe20040acdf2394
QuantumTacoNinja <worapat1h@gmail.com>
2025-12-19 10:39:23 +0700

    Update simple-deployment.sh

--- simple-deployment.sh
+++ simple-deployment.sh
@@ -7,21 +7,25 @@ NODE_VERSION="24.x"
 ssh -o StrictHostKeyChecking=no -i ${FILENAME} ${USERNAME}@${TARGET_IP} "echo hello"
 
 # Sync node packages.
-scp -o StrictHostKeyChecking=no -i ${FILENAME} index.js \${USERNAME}@target:~/index.js
-scp -o StrictHostKeyChecking=no -i ${FILENAME} package.json \${USERNAME}@target:~/package.json
-scp -o StrictHostKeyChecking=no -i ${FILENAME} package-lock.json \${USERNAME}@target:~/package-lock.json
+scp -o StrictHostKeyChecking=no -i $FILENAME index.js '$USERNAME@$TARGET_HOST:~/index.js'
+scp -o StrictHostKeyChecking=no -i $FILENAME package.json '$USERNAME@$TARGET_HOST:~/package.json'
+scp -o StrictHostKeyChecking=no -i $FILENAME package-lock.json '$USERNAME@$TARGET_HOST:~/package-lock.json'
 
 # Install nodejs and dependencies.
-ssh -o StrictHostKeyChecking=no -i ${FILENAME} ${USERNAME}@${TARGET_HOST} "
+ssh -o StrictHostKeyChecking=no -i $FILENAME '$USERNAME@$TARGET_HOST' "
   sudo apt update && sudo apt upgrade -y
+
   if ! command -v node &> /dev/null; then
     curl -fsSL https://deb.nodesource.com/setup_${NODE_VERSION} | sudo -E bash -
     sudo apt install -y nodejs
   fi
+
+  # Fix for pm2 global install permission issue (see below)
   if ! command -v pm2 &> /dev/null; then
-    sudo npm install -g pm2
+    sudo npm install -g pm2 --unsafe-perm
   fi
-  npm install -g npm@latest  # Add this line to upgrade npm
+
+  cd ~
   npm ci --omit=dev
 "
 
